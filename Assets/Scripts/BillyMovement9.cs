20f8e09aea1ed3b67848a663454c258c9f24626d
MishaDev <misael.ojeda.23@gmail.com>
2023-10-22 14:58:06 -0500

    Todos los niveles terminados

---
+++ Assets/Scripts/BillyMovement9.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillyMovement9 : MonoBehaviour
+{
+    public float runSpeed = 5f;
+    float moveHorizontal;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Flip();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MomeAxisHorizontal();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("PowerUp"))
+        {
+            runSpeed = runSpeed + 1.5f;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void MomeAxisHorizontal()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal * runSpeed, rb.velocity.y);
+        rb.velocity = movement;
+    }
+
+    public void Flip()
+    {
+        if (moveHorizontal > 0 && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1);
+        }
+        else if (moveHorizontal < 0 && transform.localScale.x > 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1);
+        }
+
+    }
+}
